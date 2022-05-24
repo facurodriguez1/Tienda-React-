db20d9cd1e381d5b111dc43c324c115642fa2515
Facundo Rodríguez <facundoomrodriguez@gmail.com>
2022-05-24 15:39:50 -0300

    Correciones entrega final

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,100 +1,126 @@
-import { useContext, useState } from "react"
-import CartContext from "../context/CartContext"
-import CartItem from "../CartItem/CartItem"
-import "./Cart.css"
-import { getDocs, writeBatch, query, where, collection, documentId, addDoc} from 'firebase/firestore'
-import { firestoreDb } from '../../services/firebase/index'
-import Form from "../Form/Form"
-
-
+import { useContext, useState } from "react";
+import CartContext from "../context/CartContext";
+import CartItem from "../CartItem/CartItem";
+import "./Cart.css";
+import {
+  getDocs,
+  writeBatch,
+  query,
+  where,
+  collection,
+  documentId,
+  addDoc,
+} from "firebase/firestore";
+import { firestoreDb } from "../../services/firebase/index";
+import Form from "../Form/Form";
 
 const Cart = () => {
-    const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [orderId, setOrderId] = useState("");
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+  });
+  const { cart, clearCart, getTotal, getQuantity } = useContext(CartContext);
 
-    const { cart, clearCart, getTotal, getQuantity } = useContext(CartContext)
+  const createOrder = () => {
+    setLoading(true);
 
-  
-   
+    const objOrder = {
+      items: cart,
+      buyer: {
+        name: userData?.name,
+        email: userData?.email,
+      },
+      total: getTotal(),
+      date: new Date(),
+    };
 
-    const createOrder = () => {
-        setLoading(true)
+    const ids = cart.map((prod) => prod.id);
 
-        const objOrder = {
-            items: cart,
-            buyer: {
-                name: '',
-                email: ''
-            },
-            total: getTotal(),
-            date: new Date()
+    const batch = writeBatch(firestoreDb);
+
+    const collectionRef = collection(firestoreDb, "products");
+
+    const outOfStock = [];
+
+    getDocs(query(collectionRef, where(documentId(), "in", ids)))
+      .then((response) => {
+        response.docs.forEach((doc) => {
+          const dataDoc = doc.data();
+          const prodQuantity = cart.find(
+            (prod) => prod.id === doc.id
+          )?.quantity;
+
+          if (dataDoc.stock >= prodQuantity) {
+            batch.update(doc.ref, { stock: dataDoc.stock - prodQuantity });
+          } else {
+            outOfStock.push({ id: doc.id, ...dataDoc });
+          }
+        });
+      })
+      .then(() => {
+        if (outOfStock.length === 0) {
+          const collectionRef = collection(firestoreDb, "orders");
+          return addDoc(collectionRef, objOrder);
+        } else {
+          return Promise.reject({
+            name: "outOfStockError",
+            products: outOfStock,
+          });
         }
+      })
+      .then(({ id }) => {
+        batch.commit();
+        setOrderId(id);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+        setShowForm(false);
+      });
+  };
 
-        const ids = cart.map(prod => prod.id)
+  if (loading) {
+    return <h1>Se esta generando su orden</h1>;
+  }
 
-        const batch = writeBatch(firestoreDb)
+  if (getQuantity() === 0) {
+    return <h1>No hay productos en el carrito</h1>;
+  }
 
-        const collectionRef = collection(firestoreDb, 'products')
-
-        const outOfStock = []
-
-
-    
-
-   
-
-        getDocs(query(collectionRef, where(documentId(), 'in', ids)))
-            .then(response => {
-                response.docs.forEach(doc => {
-                    const dataDoc = doc.data()
-                    const prodQuantity = cart.find(prod => prod.id === doc.id)?.quantity
-
-                    if(dataDoc.stock >= prodQuantity) {
-                        batch.update(doc.ref, { stock: dataDoc.stock - prodQuantity})
-                    } else {
-                        outOfStock.push({ id: doc.id, ...dataDoc })
-                    }
-                })
-            }).then(() => {
-                if(outOfStock.length === 0) {
-                    const collectionRef = collection(firestoreDb, 'orders')
-                    return addDoc(collectionRef, objOrder)
-                } else {
-                    return Promise.reject({ name: 'outOfStockError', products: outOfStock})
-                }
-            }).then(({ id }) => {
-                batch.commit()
-                console.log(`El id de la orden es ${id}`)
-            }).catch(error => {
-                console.log(error)
-            }).finally(() => {
-                setLoading(false)
-            })
-    }
-
-    if(loading) {
-        return <h1>Se esta generando su orden</h1>
-    }
-
-    if(getQuantity() === 0) {
-        return (
-            <h1>No hay productos en el carrito</h1>
-        )
-    }
-
-    return (     
+  return (
+    <>
+      {!orderId && (
         <div className="carrito">
-            <h1>Carrito</h1>
-            { cart.map(p => <CartItem key={p.id} {...p}/>) }
-            <h3>Total: ${getTotal()}</h3>
-            <div className="botones">
-            <button onClick={() => clearCart()} className="Button">Limpiar carrito</button>
-            <button onClick={() => createOrder()} className="Button">Generar Orden</button>
-            <Form/>
-
-            </div>
-
+          <h1>Carrito</h1>
+          {cart.map((p) => (
+            <CartItem key={p.id} {...p} />
+          ))}
+          <h3>Total: ${getTotal()}</h3>
+          <div className="botones">
+            <button onClick={() => clearCart()} className="Button">
+              Limpiar carrito
+            </button>
+            <button onClick={() => setShowForm(true)} className="Button">
+              Generar Orden
+            </button>
+            {showForm && (
+              <Form
+                userData={userData}
+                setUserData={setUserData}
+                createOrder={createOrder}
+              />
+            )}
+          </div>
         </div>
-    )
-}
+      )}
+      {orderId && <h1>Su orden ha sido generada con el id: {orderId}</h1>}
+    </>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,38 +1,37 @@
-
-import { useState } from 'react'
-
-const Form = () => {
-    const [input, setInput] = useState({name:''})
-    const [email, setEmail] = useState({email:''})
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(input)
-        console.log(email)
+import { useState } from "react";
+import "./Form.css";
+const Form = ({ userData, setUserData, createOrder }) => {
+  const handleKeyDown = (e) => {
+    if (e.code === "Space") {
+      e.preventDefault();
     }
+  };
 
-    const handleKeyDown = (e) => {
-        if(e.code === 'Space') {
-            e.preventDefault()
-        }
-    }
+  return (
+    <form onSubmit={createOrder}>
+     <div class="mb-3">
+         <h4>Complete sus datos por favor:</h4>
+     </div>
+     <div class="mb-3">
+      <input
+        type="text"
+        onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+        onKeyDown={handleKeyDown}
+        placeholder="Nombre"
+      />
+      </div>
+      <div class="mb-3">
+      <input
+        type="email"
+        id="exampleInputEmail1"
+        onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        onKeyDown={handleKeyDown}
+        placeholder="Email"
+      />
+      </div> 
+      <button type="submit" class="btn btn-success">Enviar</button>
+    </form>
+  );
+};
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type='text' 
-                onChange={(e) => setInput(e.target.value)}
-                onKeyDown={handleKeyDown}
-                placeholder='name' 
-            />
-             <input type='text' 
-                onChange={(e) => setEmail(e.target.value)}
-                onKeyDown={handleKeyDown} 
-                placeholder='email'
-            />
-            <button type='submit'>submit</button>
-        </form>
-    )
-}
-
-export default Form
+export default Form;
